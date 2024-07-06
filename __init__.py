4bc020e6e3768489b1690a85d8df80b7d45de4b5
simon <simon.hobbs@electrooptical.net>
2024-07-06 13:44:15 -0400

    adding precommit formatting

--- __init__.py
+++ __init__.py
@@ -1,20 +1,21 @@
-#from .kicadtestpoints import plugin
+# from .kicadtestpoints import plugin
 import pcbnew
 import re
 import datetime
 
 
-class text_by_date( pcbnew.ActionPlugin ):
-    def defaults( self ):
+class text_by_date(pcbnew.ActionPlugin):
+    def defaults(self):
         self.name = "Add date on PCB"
         self.category = "Modify PCB"
         self.description = "Automaticaly add date on an existing PCB"
 
-    def Run( self ):
+    def Run(self):
         pcb = pcbnew.GetBoard()
         for draw in pcb.m_Drawings:
-            if draw.GetClass() == 'PTEXT':
-                txt = re.sub( "\$date\$ [0-9]{4}-[0-9]{2}-[0-9]{2}",
-                             "$date$", draw.GetText() )
+            if draw.GetClass() == "PTEXT":
+                txt = re.sub(
+                    "\$date\$ [0-9]{4}-[0-9]{2}-[0-9]{2}", "$date$", draw.GetText()
+                )
             if txt == "$date$":
-                draw.SetText( "$date$ %s"%datetime.date.today() )
+                draw.SetText("$date$ %s" % datetime.date.today())
